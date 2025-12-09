e0a9cd5e6ae49b95b5a76bb97b4e6e40eabaa687
poeticAndroid <i@poeticAndroid.online>
2025-12-09 10:58:19 +0100

    service worker should install now?

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ function init() {
       try {
         const registration = await navigator.serviceWorker.register("/sw.js", {
           scope: "./",
-        });
+        })
         if (registration.installing) {
-          console.log("Service worker installing");
+          console.log("Service worker installing")
         } else if (registration.waiting) {
-          console.log("Service worker installed");
+          console.log("Service worker installed")
         } else if (registration.active) {
-          console.log("Service worker active");
+          console.log("Service worker active")
         }
       } catch (error) {
-        console.error(`Registration failed with ${error}`);
+        console.error(`Registration failed with ${error}`)
       }
     }
-  };
+  }
 
   // …
 
-  registerServiceWorker();
+  registerServiceWorker()
 }
 init()
--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1")
+  await cache.addAll(resources)
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(addResourcesToCache(["./", "./sw.js"]))
+})
+
+
 self.addEventListener("fetch", (event) => {
   caches.open("v1").then(cache => {
     cache.add(event.request)

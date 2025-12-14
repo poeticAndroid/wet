47da3bb1007484bb96a0cdc7d716704f97edbc3a
poeticAndroid <i@poeticAndroid.online>
2025-12-14 09:50:05 +0100

    one cache per worker

--- sw.js
+++ sw.js
@@ -4,16 +4,23 @@ let cachePos = 0
 setInterval(async () => {
   let url = cached[cachePos++]
   if (!url) return cachePos = cached.length
-  let cache = await caches.open("v1")
-  console.log("Fetching", url, "...")
-  cache.add(url)
+  let keys = await caches.keys()
+  for (let key of keys) {
+    let cache = await caches.open(key)
+    if (key == location.pathname) {
+      console.log("Fetching", url, "...")
+      cache.add(url)
+    } else {
+      cache.delete(url)
+    }
+  }
 }, 1024)
 
 addEventListener("activate", (event) => {
-  console.log("Activating service worker...")
+  console.log("Activating service worker...", location.pathname)
   setTimeout(async () => {
     console.log("Updating cache...")
-    let cache = await caches.open("v1")
+    let cache = await caches.open(location.pathname)
     let reqs = await cache.keys()
     for (let req of reqs) if (!cached.includes(req.url)) cached.push(req.url)
   }, 1024 * 8)
@@ -24,7 +31,7 @@ addEventListener("fetch", async (event) => {
   let url = "" + event.request.url
   if (url.includes("?clear")) {
     console.log("Cache is cleared! 💣")
-    caches.delete("v1")
+    caches.delete(location.pathname)
     cached = ["./", "./script.js", "./style.css"]
     cachePos = 0
     return event.respondWith(fetch(event.request))
